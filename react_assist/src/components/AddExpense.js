989956e4ded7d291b09c1cea8788fd38a47942e6
aryanAbhishek18 <aryan.abhishek1829@gmail.com>
2020-04-23 19:24:18 +0530

    Modified urls to https

--- react_assist/src/components/AddExpense.js
+++ react_assist/src/components/AddExpense.js
@@ -5,7 +5,7 @@ let URL;
 if (process.env.NODE_ENV === 'development') {
     URL = 'http://localhost:5000';
 } else {
-    URL = 'http://aryanabhi.in:5000';
+    URL = 'https://aryanabhi.in:5000';
 }
 
 class AddExpense extends React.Component {
