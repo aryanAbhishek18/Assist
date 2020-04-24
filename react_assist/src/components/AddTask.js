5ae56ed2c688c1a16fab193ce9e881ae063c2077
aryanAbhishek18 <aryan.abhishek1829@gmail.com>
2020-04-25 03:23:39 +0530

    Removed port no from url to setup reverse proxy

--- react_assist/src/components/AddTask.js
+++ react_assist/src/components/AddTask.js
@@ -5,7 +5,7 @@ let URL;
 if (process.env.NODE_ENV === 'development') {
     URL = 'http://localhost:5000';
 } else {
-    URL = 'https://aryanabhi.in:5000';
+    URL = 'https://aryanabhi.in';
 }
 
 class AddTask extends React.Component {
