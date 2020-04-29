c8a2afd046259dee6f5f06c790d378d034295219
aryanAbhishek18 <aryan.abhishek1829@gmail.com>
2020-04-29 05:32:57 +0530

    Fixed an error in App.js

--- react_assist/src/App.js
+++ react_assist/src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
   }
 
   signOutHandler(e) {
-    sessionStorage.removeItem('userId');
+    sessionStorage.removeItem('assistToken');
     this.setState({
       isLoggedIn: false
     });
@@ -74,66 +74,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-
-// export default function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <nav className="navbar navbar-expand-lg navbar-light">
-
-//           <a className="navbar-brand" href="#">Navbar</a>
-//           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-//             <span className="navbar-toggler-icon"></span>
-//           </button>
-
-
-//           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-//             <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-//               <li className="nav-item">
-//                 <Link to="/" className="nav-link">Home</Link>
-//               </li>
-//               <li className="nav-item">
-//               <Link to="/profile" className="nav-link">Profile</Link>
-//               </li>
-//               <li className="nav-item">
-//               <Link to="/tasks" className="nav-link">Tasks</Link>
-//               </li>
-//             </ul>
-//           </div>  
-
-//         </nav>
-
-//         {/* A <Switch> looks through its children <Route>s and
-//             renders the first one that matches the current URL. */}
-//         <Switch>
-//           <Route path="/profile">
-//             <Profile />
-//           </Route>
-//           <Route path="/tasks">
-//             <Tasks />
-//           </Route>
-//           <Route path="/">
-//             <Home />
-//           </Route>
-//         </Switch>
-//       </div>
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//       </header>
-//     </Router>
-    
-//   );
-// }
-
-// function Home() {
-//   return <h2>Hogggme</h2>;
-// }
-
-// function Profile() {
-//   return <h2>Profile</h2>;
-// }
-
-// function Tasks() {
-//   return <h2>Tasks</h2>;
-// }
